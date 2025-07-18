784dcfa12dfe416dd97a53cc00fde49701d9bf3e
aimilne <alastair@birda.org>
2025-07-18 18:18:21 +0100

    fix import on Android when there's no native module

--- src/BranchInit.ts
+++ src/BranchInit.ts
@@ -1,8 +1,12 @@
 import { requireNativeModule } from "expo-modules-core";
+import { Platform } from "react-native";
 
-const BranchInit = requireNativeModule("BranchInit");
+const BranchInit =
+  Platform.OS === "ios" ? requireNativeModule("BranchInit") : null;
 
-//iOS only
 export async function initSession(): Promise<void> {
+  if (!BranchInit) {
+    return Promise.reject(new Error("initSession is only supported on iOS"));
+  }
   return await BranchInit.initSession();
 }
