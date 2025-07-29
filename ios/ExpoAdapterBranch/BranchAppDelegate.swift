3f7b5317ab466a5f5c9327f4421c44bcf7257917
aimilne <alastair@birda.org>
2025-07-29 17:44:40 +0100

    fix logging test - remove the call to RNBranch.application

--- ios/ExpoAdapterBranch/BranchAppDelegate.swift
+++ ios/ExpoAdapterBranch/BranchAppDelegate.swift
@@ -7,13 +7,14 @@ public class BranchAppDelegate: ExpoAppDelegateSubscriber {
 
   public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     BranchAppDelegate.cachedLaunchOptions = launchOptions
-    os_log("BranchAppDelegate: Launch options: %{public}%@ cached successfully", type: .info, String(describing: launchOptions))
+    os_log("BranchAppDelegate: Launch options: %{public}@ cached successfully", type: .info, String(describing: launchOptions))
     return true
   }
 
   public func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
     os_log("BranchAppDelegate: open url: %{public}@", type: .info, url.absoluteString)
-    return RNBranch.application(application, open:url, options:options)
+    // return RNBranch.application(application, open:url, options:options)
+    return false
   }
 
   public func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
