591860e4587636d41f29f2d971d0e8fcd6b3e435
aimilne <alastair@birda.org>
2025-07-31 18:10:06 +0100

    return false from open via url callback

--- ios/ExpoAdapterBranch/BranchAppDelegate.swift
+++ ios/ExpoAdapterBranch/BranchAppDelegate.swift
@@ -14,7 +14,7 @@ public class BranchAppDelegate: ExpoAppDelegateSubscriber {
   public func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
     os_log("BranchAppDelegate: open url: %{public}@", type: .info, url.absoluteString)
 //    return RNBranch.application(application, open:url, options:options)
-    return true
+    return false
   }
 
   public func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
